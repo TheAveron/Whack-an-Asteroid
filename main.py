4b661306a3705dc03aadc70fee056479792474a5
Averon <77922089+TheAveron@users.noreply.github.com>
2023-09-27 17:22:00 +0200

    feat: updating some things

--- main.py
+++ main.py
@@ -3,22 +3,20 @@ import pygame, json
 # Lancement de pygame
 pygame.init()
 
-
 # Vérification de si c'est la première fois que le jeu est lancé
 try:
-	# Si ce n'est pas la première fois, un fichier "data.json" devrai exister
-	f=open('data.json')
-	f.close()
+    # Si ce n'est pas la première fois, un fichier "data.json" devrai exister
+    f = open("data.json")
+    f.close()
 except FileNotFoundError:
-	# Si le fichier n'existe pas, c'est que c'est la première fois que le jeu est lancé, ainsi on charge les données par défaut
-	with open('Static/Base_data/data_default.json',"r") as f1:
-		data=json.load(f1)
-		with open('data.json','w') as f2:
-			json.dump(data, f2, indent=4)
+    # Si le fichier n'existe pas, c'est que c'est la première fois que le jeu est lancé, ainsi on charge les données par défaut
+    with open("Static/Base_data/data_default.json", "r") as f1:
+        data = json.load(f1)
+        with open("data.json", "w") as f2:
+            json.dump(data, f2, indent=4)
 
 # Importation des différentes classes/fonctions/variables du jeu
-from game_files import *
+from src import *
 
 # Lancement du menu du jeu
 menu()
-
